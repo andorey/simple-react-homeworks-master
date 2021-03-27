98276d382c8fe65e97add4970a4448b7b7c349c7
andoreyrich <andoreyrich@gmail.com>
2021-03-27 20:52:07 +0100

    homework 4 - input, button animation

--- src/p2-homeworks/h4/common/c1-SuperInputText/AlternativeSuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/AlternativeSuperInputText.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function AlternativeSuperInputText() {
+export function AlternativeSuperInputText() {
     return (
         <input/>
     )
 }
 
-export default AlternativeSuperInputText
+
